c5c0701c86982f0e97bf9d2ce91964cc37b30cef
gustavg <gustav.gamstedt@outlook.com>
2026-01-16 16:12:20 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My first flake!";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+  outputs = {self, nixpkgs, nixpkgs_unstable, home-manager, ...}:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs_unstable.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      gustav = lib.nixosSystem {
+        inherit system;
+	modules = [ ./configuration.nix ];
+	specialArgs = {
+          inherit pkgs-unstable;
+	};
+      };
+    };
+    homeConfigurations = {
+      gustav = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs; # Som input = input.
+	modules = [ ./home.nix ];
+	extraSpecialArgs = {
+          inherit pkgs-unstable;
+	};
+      };
+    };
+
+  };
+}
