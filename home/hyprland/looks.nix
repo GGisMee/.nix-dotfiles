e437f70f9cd3286cf7a2f4d13e7667d14dc684f4
gustavg <gustav.gamstedt@outlook.com>
2026-01-19 19:06:33 +0100

    added more decoration

--- home/hyprland/looks.nix
+++ home/hyprland/looks.nix
@@ -1,15 +1,28 @@
 {
-  wayland.windowManager.hyprland.settings.animations = {
-    enabled = true;
-    bezier = "myBezier, 0.075, 0.7, 0.075, 1.05";
+  wayland.windowManager.hyprland.settings = {
 
-    animation = [
-      "windows, 1, 7, myBezier"
-      "windowsOut, 1, 7, default, popin 70%"
-      "border, 1, 10, default"
-      "borderangle, 1, 8, default"
-      "fade, 1, 2, default"
-      "workspaces, 1, 6, default"
-    ];
+    animations = {
+      enabled = true;
+      bezier = "myBezier, 0.075, 0.7, 0.075, 1.05";
+
+      animation = [
+        "windows, 1, 5, myBezier"
+        "windowsOut, 1, 5, default, popin 70%"
+        "border, 1, 5, default"
+        "borderangle, 1, 5, default"
+        "fade, 1, 2, default"
+        "workspaces, 1, 5, default"
+      ];
+    };
+
+    decoration = {
+      rounding = 6;
+
+      blur = {
+        enabled = true;
+        size = 9;
+        passes = 2;
+      };
+    };
   };
 }
