53fd801f11afa61146ef6f92716149d74a1185f7
gustavg <gustav.gamstedt@outlook.com>
2026-01-28 21:20:47 +0100

    change to simpel waybar

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -4,23 +4,43 @@ programs.waybar = {
   enable = true;
   settings = {
     mainBar = {
-      layer = "top";
-      modules-left = [ "hyprland/workspaces" ];
-      # ... resten av din config
+    position = "top";
+
+    modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+    modules-center = [ "clock" ];
+    modules-right = [ "pulseaudio" "network" "cpu" "memory" "tray" "battery"];
+    
+    "battery" = {
+      states = {
+        warning = 30;
+        critical = 15;
+      };
+
+      format = "{icon} {capacity}%";
+      format-charging = " {capacity}%";
+      format-plugged = " {capacity}%";
+      format-icons = [ "" "" "" "" "" ];
+    };
+
+    "hyprland/workspaces" = {
+      format = "{name}";
+      on-click = "activate";
+      all-outputs = true;
+    };
+  
+    "clock" = {
+      format = "{:%H:%M | %d/%m}";
+      tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+    };
+
+    "pulseaudio" = {
+      format = "{icon} {volume}%";
+      format-icons = { default = [ "" "" "" ]; };
+      on-click = "pavucontrol";
+      };
     };
   };
-  
   # HÄR IMPORTERAS STYLINGEN:
-  style = ''
-    window#waybar {
-      background: @base00; /* Hämtas direkt från Stylix */
-      border-bottom: 3px solid @base0E;
-      color: @base05;
-    }
-    #workspaces button.active {
-      background: @base0E;
-      color: @base00;
-    }
-  '';
-};
+  style = builtins.readFile ./style.css;
+  };
 }
